0332ddb1619293bb59c4bfe9ece8b56820c6cbcf
Dimas Arya Pratama <145015256+Dimasarya-23@users.noreply.github.com>
2024-07-04 19:41:14 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Product data
+const products = [
+    {
+        id: 1,
+        name: "Dompet 1",
+        image: "dompet1.jpg",
+        price: 100000,
+        description: "This is a description of Dompet 1"
+    },
+    {
+        id: 2,
+        name: "Dompet 2",
+        image: "dompet2.jpg",
+        price: 200000,
+        description: "This is a description of Dompet 2"
+    },
+    // Add more products here
+];
+
+// Render product cards on all-products.html
+document.addEventListener("DOMContentLoaded", () => {
+    const productGrid = document.querySelector(".
